3e290f4976edf70d87e676991c80d9edb0db54ff
ext_em <edouard.misset@intest.info>
2023-03-06 16:13:42 +0100

    Mock a JavaScript module in a test

--- src/no-framework/inline-module-mock.js
+++ src/no-framework/inline-module-mock.js
@@ -4,18 +4,30 @@
  * Execute: Use `npx jest --watch src/no-framework/inline-module-mock.js` to watch the test
  */
 
+
 function fn(impl = () => {}) {
   const mockFn = (...args) => {
     mockFn.mock.calls.push(args)
     return impl(...args)
   }
-  mockFn.mock = {calls: []}
+  mockFn.mock = { calls: [] }
   return mockFn
+}
+
+const utilsPath = require.resolve('../utils')
+require.cache[utilsPath] = {
+  id: utilsPath,
+  filename: utilsPath,
+  loaded: true,
+  exports: {
+    getWinner: fn((p1, p2) => p1)
+  }
 }
 
 const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
+
 
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
@@ -25,7 +37,7 @@ assert.deepStrictEqual(utils.getWinner.mock.calls, [
 ])
 
 // cleanup
-
+delete require.cache[utilsPath]
 /**
  * Hints:
  * - https://nodejs.org/api/modules.html#modules_caching
